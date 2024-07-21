152893efcadd7a959df450380c374d7ed13e564f
Nicolas Nieto <nicolasnieto82@hotmail.com>
2024-07-21 13:54:39 +0200

    functions-and-array-done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,9 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(a, b) {
+    if (a > b) {
+        return a;
+    }
+}
 
 
 
@@ -7,7 +11,22 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+
+    if (words.length === 0) {
+        return null;
+    }
+
+    let longestWord = [0];
+
+    for (let i = 0; i < words.length; i++) {
+
+        if (longestWord.length < words[i].length) {
+            longestWord = words[i];
+        }
+    }
+    return longestWord;
+}
 
 
 
@@ -15,7 +34,14 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let sum = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
 
 
 
@@ -23,7 +49,12 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    if (numbers2.length === 0) {
+        return 0;
+    }
+    return sumNumbers(numbers2) / numbers2.length;
+}
 
 
 
@@ -31,4 +62,21 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2,wordToSearch) {
+    if (words2.length === 0||wordToSearch.length=== 0){
+        return null;
+    }
+    else if (wordToSearch.length === 0 && words2.length === 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+
+
+
+
+
+
+
+ }
